78ac64c947dfee06fe163964e42a6d96961f9dcf
natthn <nathanbezgui@gmail.com>
2023-06-22 02:12:55 -0300

    Update

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+function clickMenu(){
+    if (itens.style.display == 'block'){
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+}
+
+function resize(){
+    if(window.innerWidth >= 768) {
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+}
